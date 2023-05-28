93b0f47dae89fbf2b44740cacf18c0b2d2b1dc52
AJHA96 <117764226+AJHA96@users.noreply.github.com>
2023-05-28 12:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const input= document.querrySelectorAll("input");
+
+inputs.forEach((input, index1)) =>{
+	input.addEventListner("keyup", (e) => {
+		const currentInput = input,
+			nextInput= input.nextElementSibling,
+			prevInput = input.previousElementSibling;
+		if(currentInput.value.length > 1){
+			currentInput.value= "";
+			return;
+		}
+
+		if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value!== ""){
+			nextInput.removeAttribute("disabled");
+			nextInput.focus
+		}
+		if(e.key === "Backspace"){
+			inputs.forEach((input, index2) =>{
+				if(index1 <= index2 && prevInput)
+					
+			})
+		}
+	})
+}
